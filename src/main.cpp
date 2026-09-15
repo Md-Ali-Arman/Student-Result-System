7c0be564dcb701c8aba108d88afcba696c2fc5e3
Md Ali Arman <mdaliarman16@gmail.com>
2026-09-15 17:42:13 +0600

    Change title in main

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    cout << "=== Student Result Management System ===" << endl;
+    cout << "=== DIU Student Result System ===" << endl;
     cout<< "Feature: Add Student" << endl;
     return 0;
 }
